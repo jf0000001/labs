6b030b666eb0003976d69d7df9c4f9c1a7238399
JF <joshuaf1972@gmail.com>
2026-09-02 12:26:03 -0400

    Finish WASD movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class MovementController : MonoBehaviour
 {
-    public float speed = 5f;
+    public float moveSpeed = 5f;
 
     void Start()
     {
@@ -12,9 +12,15 @@ public class MovementController : MonoBehaviour
 
     void Update()
     {
-        if (Keyboard.current.spaceKey.isPressed)
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
+        Keyboard input = Keyboard.current;
+        if (input == null) { return; }
+
+        Vector3 direction = new Vector3(
+            (input.dKey.isPressed ? 1 : 0) - (input.aKey.isPressed ? 1 : 0),
+            0,
+            (input.wKey.isPressed ? 1 : 0) - (input.sKey.isPressed ? 1 : 0)
+        ); // Get vector representing direction for movement
+
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
     }
 }
